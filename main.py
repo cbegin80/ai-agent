645f94e13dd14bf744d1a1ea5d0a98dd520b97d0
cbegin80 <cbegin80@gmail.com>
2026-04-25 19:00:30 +0000

    Add verbose output option

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import argparse
 
 def main():
     # get prompt from command line arguments
-    parser = argparse.ArgumentParser(description = "Chatbot")
-    parser.add_argument("user_prompt", type = str, help = "User prompt")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument(
+        "--verbose", action="store_true", help="Enable verbose output"
+    )
     args = parser.parse_args()
+    
+    # create context 
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     # connect to Gemini
     load_dotenv() #get API key
     api_key = os.environ.get("GEMINI_API_KEY")
+    
     # check if the API key was found
     if api_key == None:
         raise RuntimeError("API Key not found.")
 
     client = genai.Client(api_key = api_key)
     prompt = args.user_prompt
+    verbose = args.verbose
 
-    response = client.models.generate_content(model = "gemini-2.5-flash", contents = prompt)
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash", contents = prompt
+    )
     
-    # print metadata
-    if response.usage_metadata:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        raise RuntimeError("No response received from API")
+    # print metadata if requested
+    if verbose:
+        print(f"User prompt: {prompt}")
+        if response.usage_metadata:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        else:
+            raise RuntimeError("No response received from API")
 
     print(response.text)
 
