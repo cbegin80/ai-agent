d38fecafc10c5fdca9f3d0282d892a7977d2c9a5
cbegin80 <cbegin80@gmail.com>
2026-04-25 12:59:58 +0000

    add dynamic prompt from command line

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import argparse
 
 def main():
-    load_dotenv()
+    # get prompt from command line arguments
+    parser = argparse.ArgumentParser(description = "Chatbot")
+    parser.add_argument("user_prompt", type = str, help = "User prompt")
+    args = parser.parse_args()
+    
+    # connect to Gemini
+    load_dotenv() #get API key
     api_key = os.environ.get("GEMINI_API_KEY")
     # check if the API key was found
     if api_key == None:
         raise RuntimeError("API Key not found.")
 
     client = genai.Client(api_key = api_key)
+    prompt = args.user_prompt
 
-    response = client.models.generate_content(model = "gemini-2.5-flash", contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    response = client.models.generate_content(model = "gemini-2.5-flash", contents = prompt)
     
     # print metadata
     if response.usage_metadata:
