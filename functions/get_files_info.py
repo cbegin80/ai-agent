6e827922b949e91bbdb03130c00ed78ddc0aab33
cbegin80 <cbegin80@gmail.com>
2026-04-25 22:54:20 +0000

    Complete directory content function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,29 +1,32 @@
 import os
 
 def get_files_info(working_directory, directory='.'):
-    wd_abs_path = os.path.abspath(working_directory)
-    target_dir_path = os.path.normpath(os.path.join(wd_abs_path, directory))
-    
-    # validate directory path provided
-    valid_target_dir = os.path.commonpath([wd_abs_path, target_dir_path]) == wd_abs_path
-
-    if not valid_target_dir:
-        return(f'Error: Cannot list "{directory}" as it is outisde the permitted working directory')
-
-    if not os.path.isdir(target_dir_path):
-        return (f'Error: "{directory}" is not a directory')
-    
-    # print directory contents
-    
-    dir_list = os.listdir(target_dir_path)
-    output = ""
-    for item in dir_list:
-        item_path = os.path.normpath(os.path.join(target_dir_path, item))
-        item_size = os.path.getsize(item_path)
+    try:
+        wd_abs_path = os.path.abspath(working_directory)
+        target_dir_path = os.path.normpath(os.path.join(wd_abs_path, directory))
         
-        output +=   (   
-                        f"- {item}: file_size={os.path.getsize(item_path)},"
-                        f"is_dir={os.path.isdir(item_path)}\n"
-                    )
+        # validate directory path provided
+        valid_target_dir = os.path.commonpath([wd_abs_path, target_dir_path]) == wd_abs_path
+
+        if not valid_target_dir:
+            return(f'Error: Cannot list "{directory}" as it is outisde the permitted working directory')
+
+        if not os.path.isdir(target_dir_path):
+            return (f'Error: "{directory}" is not a directory')
         
-    print(output)
+        # print directory contents
+        
+        dir_list = os.listdir(target_dir_path)
+        output = ""
+        for item in dir_list:
+            item_path = os.path.normpath(os.path.join(target_dir_path, item))
+            item_size = os.path.getsize(item_path)
+            
+            output +=   (   
+                            f"- {item}: file_size={os.path.getsize(item_path)}"
+                            f" bytes, is_dir={os.path.isdir(item_path)}\n"
+                        )
+    except Exception as e:
+        output = f"Error: {e}"        
+    
+    return output
