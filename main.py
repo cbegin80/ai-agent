43e3df05949be8967c71abf1000ea00abcf5f357
cbegin80 <cbegin80@gmail.com>
2026-05-29 05:38:52 +0000

    add function_calling logic

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from google import genai
 from google.genai import types
 import argparse
 from prompts import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 
 def main():
     # get prompt from command line arguments
@@ -56,9 +56,25 @@ def main():
     if type(response.function_calls) == list:
         # print the list of function calls
         for function_call in response.function_calls:
-            print(
-                f"Calling function: {function_call.name}({function_call.args})"
-                )
+           
+            function_call_result = call_function(function_call, verbose)
+            # print(function_call_result)                                                     #TRACE
+            # print(function_call_result.parts)                                               #TRACE
+
+            # check that response is valid
+            if (not isinstance(function_call_result.parts, list) or
+                function_call_result.parts == None or 
+                function_call_result.parts == [] or 
+                function_call_result.parts[0].function_response == None or
+                function_call_result.parts[0].function_response.response == None
+            ):
+                raise Exception("Function call did not return a valid "
+                                "response.")
+            
+            function_results = [function_call_result.parts[0]]
+        
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
     else:
         print(response.text)
 
